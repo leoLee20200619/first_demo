20ed7a0614ac15d3e56e0684d914a9d9d092569d
leo.li <leo.li@ambergroup.io>
2020-06-19 11:58:10 +0800

    test_commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,212 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <a-button class="editable-add-btn" @click="handleAdd">
+      AddQ
+    </a-button>
+    <a-table :columns="columns" :data-source="data" bordered>
+      <template
+              v-for="col in ['name', 'age', 'address']"
+              :slot="col"
+              slot-scope="text, record"
+      >
+        <div :key="col">
+          <a-input
+                  v-if="record.editable"
+                  style="margin: -5px 0"
+                  :value="text"
+                  @change="e => handleChange(e.target.value, record.key, col)"
+          />
+          <template v-else>
+            {{ text }}
+          </template>
+        </div>
+      </template>
+      <template slot="name" slot-scope="text, record">
+        <editable-cell :text="text" @change="handleChange(record.key, 'name', $event)" />
+      </template>
+      <template slot="operation" slot-scope="text, record">
+        <div class="editable-row-operations">
+        <span v-if="record.editable">
+          <a @click="() => save(record.key)">Save</a>
+          <a-popconfirm
+                  v-if="data.length"
+                  title="Sure to delete?"
+                  @confirm="() => onDelete(record.key)"
+          >
+            <a href="javascript:;">Delete</a>
+          </a-popconfirm>
+          <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
+            <a>Cancel</a>
+          </a-popconfirm>
+        </span>
+          <span v-else>
+          <a :disabled="editingKey !== ''" @click="() => edit(record.key)">Edit</a>
+        </span>
+        </div>
+      </template>
+    </a-table>
   </div>
+
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const columns = [
+    {
+      title: 'name',
+      dataIndex: 'name',
+      width: '25%',
+      scopedSlots: { customRender: 'name' },
+    },
+    {
+      title: 'age',
+      dataIndex: 'age',
+      width: '15%',
+      scopedSlots: { customRender: 'age' },
+    },
+    {
+      title: 'address',
+      dataIndex: 'address',
+      width: '40%',
+      scopedSlots: { customRender: 'address' },
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      scopedSlots: { customRender: 'operation' },
+    },
+  ];
+  const data = [];
+  for (let i = 0; i < 5; i++) {
+    data.push({
+      key: i.toString(),
+      name: `Edrward ${i}`,
+      age: 32,
+      address: `London Park no. ${i}`,
+    });
   }
-}
+  import EditableCell from './edit.vue'
+  export default {
+    components: {
+      EditableCell,
+    },
+    data() {
+      this.cacheData = data.map(item => ({ ...item }));
+      return {
+        data,
+        columns,
+        editingKey: '',
+        count: data.length,
+      };
+    },
+    methods: {
+      handleChange(value, key, column) {
+        const newData = [...this.data];
+        const target = newData.filter(item => key === item.key)[0];
+        if (target) {
+          target[column] = value;
+          this.data = newData;
+        }
+      },
+      onDelete(key) {
+        const newData = [...this.data];
+        this.data = newData.filter(item => item.key !== key);
+        this.editingKey = '';
+        const target = newData.filter(item => key === item.key)[0];
+        if (target) {
+          Object.assign(target, this.cacheData.filter(item => key === item.key)[0]);
+          delete target.editable;
+        }
+      },
+      handleAdd() {
+        const { count, data } = this;
+        const newData = {
+          key: count,
+          name: `Edward King ${count}`,
+          age: 777,
+          address: `London, Park Lane no. ${count}`,
+        };
+        this.data = [...data, newData];
+        this.count = count + 1;
+      },
+      edit(key) {
+        const newData = [...this.data];
+        const target = newData.filter(item => key === item.key)[0];
+        this.editingKey = key;
+        if (target) {
+          target.editable = true;
+          this.data = newData;
+        }
+      },
+      save(key) {
+        const newData = [...this.data];
+        const newCacheData = [...this.cacheData];
+        const target = newData.filter(item => key === item.key)[0];
+        const targetCache = newCacheData.filter(item => key === item.key)[0];
+        if (target && targetCache) {
+          delete target.editable;
+          this.data = newData;
+          Object.assign(targetCache, target);
+          this.cacheData = newCacheData;
+        }
+        this.editingKey = '';
+      },
+      cancel(key) {
+        const newData = [...this.data];
+        const target = newData.filter(item => key === item.key)[0];
+        this.editingKey = '';
+        if (target) {
+          Object.assign(target, this.cacheData.filter(item => key === item.key)[0]);
+          delete target.editable;
+          this.data = newData;
+        }
+      },
+    },
+  };
 </script>
+<style scoped>
+  .editable-cell {
+    position: relative;
+  }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .editable-cell-input-wrapper,
+  .editable-cell-text-wrapper {
+    padding-right: 24px;
+  }
+
+  .editable-cell-text-wrapper {
+    padding: 5px 24px 5px 5px;
+  }
+
+  .editable-cell-icon,
+  .editable-cell-icon-check {
+    position: absolute;
+    right: 0;
+    width: 20px;
+    cursor: pointer;
+  }
+
+  .editable-cell-icon {
+    line-height: 18px;
+    display: none;
+  }
+
+  .editable-cell-icon-check {
+    line-height: 28px;
+  }
+
+  .editable-cell:hover .editable-cell-icon {
+    display: inline-block;
+  }
+
+  .editable-cell-icon:hover,
+  .editable-cell-icon-check:hover {
+    color: #108ee9;
+  }
+
+  .editable-add-btn {
+    margin-bottom: 8px;
+  }
+
+  .editable-row-operations a {
+    margin-right: 8px;
+  }
 </style>
